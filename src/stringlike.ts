6c28b323e550aedb7260e04d7b4ba342f0697921
Sergey Grabkovsky <grabkovsky.s@gmail.com>
2018-09-07 21:52:55 -0400

    Extend StringLike

--- src/stringlike.ts
+++ src/stringlike.ts
@@ -4,8 +4,12 @@ interface IBaseStringLike {
 }
 
 export interface StringLike extends IBaseStringLike {
+  length: number;
+  rangeOnUnderlying(): {start: number, end: number}
   getString(): string
   char(index: number): string|null
+  charSlice(index: number): StringLike|null
+  slice(start: number, end: number): StringLike
 }
 export namespace StringLike {
   export function wrap(str: string): StringLike {
@@ -17,9 +21,25 @@ export namespace StringLike {
 }
 
 abstract class BaseStringLike implements StringLike {
+  abstract length: number;
   abstract getString(): string
   abstract char(index: number, start?: number): string|null
   abstract indexOf(contained: string, start?: number): number
+  abstract rangeOnUnderlying(): {start: number, end: number}
+  protected abstract getUnderlying(): string
+  charSlice(index: number): StringLike|null {
+    const slice = this.slice(index, index + 1);
+    if (slice.length === 0) {
+      return null;
+    }
+    return slice;
+  }
+  slice(start: number, end: number): StringLike {
+    const range = this.rangeOnUnderlying();
+    start = Math.max(start + range.start, range.start);
+    end = Math.min(end + range.start, range.end);
+    return StringLike.slice(this.getUnderlying(), start, end);
+  }
   findIndices(contained: string, start: number = 0) {
     const indices: number[] = [];
     while (true) {
@@ -37,8 +57,20 @@ abstract class BaseStringLike implements StringLike {
 
 class WrappedString extends BaseStringLike implements StringLike {
   constructor(private underlying: string) { super() }
+  get length(): number {
+    return this.underlying.length;
+  }
   indexOf(contained: string, start?: number): number {
     return this.underlying.indexOf(contained, start);
+  }
+  protected getUnderlying(): string {
+    return this.underlying;
+  }
+  rangeOnUnderlying(): {start: number, end: number} {
+    return {
+      start: 0,
+      end: this.underlying.length
+    };
   }
   getString(): string { return this.underlying }
   char(index: number): string|null {
@@ -57,6 +89,18 @@ class StringSlice extends BaseStringLike implements StringLike {
     super();
     this.start = Math.max(Math.min(start, underlying.length - 1), 0);
     this.end   = Math.max(Math.min(end,   underlying.length),     0);
+  }
+  get length(): number {
+    return this.end - this.start;
+  }
+  protected getUnderlying(): string {
+    return this.underlying;
+  }
+  rangeOnUnderlying(): {start: number, end: number} {
+    return {
+      start: this.start,
+      end: this.end
+    };
   }
   indexOf(contained: string, start?: number): number {
     const result = this.underlying.indexOf(contained, start);
